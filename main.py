a4c8cd92298012ec922350f3f6bb92e9581175cf
ShOOmet14 <walusradoslaw@gmail.com>
2026-05-08 22:08:24 +0200

    Created option to see differences between no drag and linear drag for plotting.

--- main.py
+++ main.py
@@ -13,21 +13,15 @@ from config.parameters import (
     LINEAR_DRAG_COEFFICIENT,
     T_MAX,
 )
-# from physics.no_drag import (
-#     calculate_position_no_drag,
-#     calculate_velocity_no_drag,
-#     calculate_flight_time_no_drag,
-#     calculate_max_height_no_drag,
-#     calculate_range_no_drag,
-#     calculate_time_to_max_height_no_drag
-# )
+
+from physics.no_drag import calculate_position_no_drag, calculate_velocity_no_drag
 
 from physics.linear_drag import (
     calculate_position_linear_drag,
     calculate_velocity_linear_drag,
 )
 
-# from visualization.plots import plot_motion_no_drag
+from visualization.plots import plot_motion
 from physics.interpolation import interpolate_ground_hit
 
 if __name__ == "__main__":
@@ -47,34 +41,67 @@ if __name__ == "__main__":
     time = np.arange(0.0, T_MAX, DT)
 
     linear_drag_const = LINEAR_DRAG_COEFFICIENT / MASS
-    x, y = calculate_position_linear_drag(time, X0, Y0, VX0, VY0, G, linear_drag_const)
-    vx, vy = calculate_velocity_linear_drag(time, VX0, VY0, G, linear_drag_const)
+    x_linear, y_linear = calculate_position_linear_drag(
+        time, X0, Y0, VX0, VY0, G, linear_drag_const
+    )
+    vx_linear, vy_linear = calculate_velocity_linear_drag(
+        time, VX0, VY0, G, linear_drag_const
+    )
+
+    x_no_drag, y_no_drag = calculate_position_no_drag(time, X0, Y0, VX0, VY0, G)
+    vx_no_drag, vy_no_drag = calculate_velocity_no_drag(time, VX0, VY0, G)
 
     # making a logical mask to cut off all recorts where y < 0
-    first_negative_index = np.where(y < 0)[0][0]
-    first_negative_index = int(first_negative_index)
+    first_negative_index_no_drag = np.where(y_no_drag < 0)[0][0]
+    first_negative_index_no_drag = int(first_negative_index_no_drag)
 
-    x_hit, t_hit = interpolate_ground_hit(
-        time[first_negative_index - 1],
-        x[first_negative_index - 1],
-        y[first_negative_index - 1],
-        time[first_negative_index],
-        x[first_negative_index],
-        y[first_negative_index],
+    first_negative_index_linear = np.where(y_linear < 0)[0][0]
+    first_negative_index_linear = int(first_negative_index_linear)
+
+    x_hit_linear, t_hit_linear = interpolate_ground_hit(
+        time[first_negative_index_linear - 1],
+        x_linear[first_negative_index_linear - 1],
+        y_linear[first_negative_index_linear - 1],
+        time[first_negative_index_linear],
+        x_linear[first_negative_index_linear],
+        y_linear[first_negative_index_linear],
     )
-    mask = y >= 0
-    time = time[mask]
-    x = x[mask]
-    y = y[mask]
-    vx = vx[mask]
-    vy = vy[mask]
+
+    x_hit_no_drag, t_hit_no_drag = interpolate_ground_hit(
+        time[first_negative_index_no_drag - 1],
+        x_no_drag[first_negative_index_no_drag - 1],
+        y_no_drag[first_negative_index_no_drag - 1],
+        time[first_negative_index_no_drag],
+        x_no_drag[first_negative_index_no_drag],
+        y_no_drag[first_negative_index_no_drag],
+    )
+
+    mask_linear = y_linear >= 0
+    time_linear = time[mask_linear]
+    x_linear = x_linear[mask_linear]
+    y_linear = y_linear[mask_linear]
+    vx_linear = vx_linear[mask_linear]
+    vy_linear = vy_linear[mask_linear]
+
+    mask_no_drag = y_no_drag >= 0
+    time_no_drag = time[mask_no_drag]
+    x_no_drag = x_no_drag[mask_no_drag]
+    y_no_drag = y_no_drag[mask_no_drag]
+    vx_no_drag = vx_no_drag[mask_no_drag]
+    vy_no_drag = vy_no_drag[mask_no_drag]
 
     print(f"k = {linear_drag_const} 1/s")
-    print(f"interpolated flight time = {t_hit} ")
-    print(f"interpolated range = {x_hit} ")
-    print(f"max_height = {max(y)} ")
+    print(f"interpolated linear flight time = {t_hit_linear} ")
+    print(f"interpolated linear range = {x_hit_linear} ")
+    print(f"max_height linear = {max(y_linear)} ")
+    print(f"interpolated no drag flight time = {t_hit_no_drag} ")
+    print(f"interpolated no drag range = {x_hit_no_drag} ")
+    print(f"max_height no drag = {max(y_no_drag)} ")
 
-    max_height_index = np.argmax(y)
-    print(f"time for max height: {time[max_height_index]}")
+    max_height_index_linear = np.argmax(y_linear)
+    print(f"time for max height linear: {time[max_height_index_linear]}")
 
-    # plot_motion_no_drag(x, y, "Projectile motion without air resistance.")
+    max_height_index_no_drag = np.argmax(y_no_drag)
+    print(f"time for max height no drag: {time[max_height_index_no_drag]}")
+
+    plot_motion(x_no_drag, y_no_drag, x_linear, y_linear)
--- visualization/plots.py
+++ visualization/plots.py
@@ -3,16 +3,22 @@ import numpy.typing as npt
 import matplotlib.pyplot as plt
 
 
-def plot_motion_no_drag(
-    x: npt.NDArray[np.float64], y: npt.NDArray[np.float64], title: str
+def plot_motion(
+    x_no_drag: npt.NDArray[np.float64],
+    y_no_drag: npt.NDArray[np.float64],
+    x_linear: npt.NDArray[np.float64],
+    y_linear: npt.NDArray[np.float64],
 ) -> None:
     _, axis = plt.subplots()
 
-    axis.plot(x, y)
-    axis.set_title(title)
+    axis.plot(x_no_drag, y_no_drag, label="No drag", color="red")
+    axis.plot(x_linear, y_linear, label="Linear drag", color="blue")
+
+    axis.set_title("Linear drag against no drag in projectile motion of a sphere.")
     axis.set_xlabel("x [m]")
     axis.set_ylabel("y [m]")
-    axis.legend(title="No drag")
+
+    axis.legend()
     axis.grid(True)
 
     plt.show()
