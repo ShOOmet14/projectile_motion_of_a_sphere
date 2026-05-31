f4370e11d3aec7b889198c0c13a6229722b5029b
ShOOmet14 <walusradoslaw@gmail.com>
2026-05-31 12:05:08 +0200

    refactor(main): refactor of main.py with the purpose of future unit tests creation

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import sys
 
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
-
-from src.config.settings import get_stylesheet, load_theme
-from src.gui.main_window import MainWindow
+from typing import NoReturn
 
 
-if __name__ == "__main__":
+APP_ICON_PATH = "style/icons/app_icon.svg"
+
+
+def main() -> NoReturn:
+    from PySide6.QtGui import QIcon
+    from PySide6.QtWidgets import QApplication
+
+    from src.config.settings import get_stylesheet, load_theme
+    from src.gui.main_window import MainWindow
+
     app = QApplication(sys.argv)
 
-    app.setWindowIcon(QIcon("style/icons/app_icon.svg"))
+    app.setWindowIcon(QIcon(APP_ICON_PATH))
     app.setStyleSheet(get_stylesheet(load_theme()))
 
     window = MainWindow()
-    window.setWindowIcon(QIcon("style/icons/app_icon.svg"))
+    window.setWindowIcon(QIcon(APP_ICON_PATH))
     window.showMaximized()
 
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
